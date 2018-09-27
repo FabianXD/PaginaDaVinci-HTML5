fb7267ab97a7d0c9446bd465bdfc32dfdce69d9b
FabianXD <fabianma7@gmail.com>
2018-09-27 13:06:04 -0300

    index min

--- js/plugins.js
+++ js/plugins.js
@@ -1,24 +1,22 @@
-// Avoid `console` errors in browsers that lack a console.
+// Evite los errores de `console` en los navegadores que carecen de una consola.
 (function() {
-  var method;
-  var noop = function () {};
-  var methods = [
-    'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-    'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-    'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-    'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
-  ];
-  var length = methods.length;
-  var console = (window.console = window.console || {});
-
-  while (length--) {
-    method = methods[length];
-
-    // Only stub undefined methods.
-    if (!console[method]) {
-      console[method] = noop;
+    var method;
+    var noop = function() {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+    while (length--) {
+        method = methods[length];
+        // Solo trozo de métodos no definidos.
+        if (!console[method]) {
+            console[method] = noop;
+        }
     }
-  }
 }());
 
-// Place any jQuery/helper plugins in here.
+// Coloque cualquier complemento jQuery / helper aquí.
