81e2ff2d6035000f32ee9c4b5676db488a371028
FabianXD <fabianma7@gmail.com>
2018-09-27 13:25:58 -0300

    camvio de ubicasiones

---
+++ INFORMACION/header1.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html lang="es">
+	<head>
+		<meta charset="utf-8">
+		<title>Leonardo Da Vinci School&middot;Pitrufquen</title><!--titulo de la pagina-->
+		<link rel="stylesheet" href="estilos_css.css"><!--pagina de estilos-->
+		<link href="img/favicon.jpg" rel="icon" type="image/x-icon" /><!--icono en barra de navegador-->
+		</head>
+	<body>
+		<table class="center"><!--tabla redondeada-->
+			<tbody>
+				<tr>
+					<td>
+						<div id="menu">
+							<ul class="navtop">
+								<li><img src="img/logo1.jpg" width="146" class="imgmenu14" alt="error"></li>
+								<li><a href="#">&nbsp;Portada</a></li>
+								<li><a href="#">&nbsp;&nbsp;Leonardo Da Vinci School</a>
+									<ul>
+										<li><a href="QUIENES_SOMOS">&nbsp;&bull;&nbsp;&nbsp;Quienes Somos</a></li>
+										<li><a href="HISTORIAS">&nbsp;&bull;&nbsp;&nbsp;Historia</a></li>
+										<li><a href="MENSAJES">&nbsp;&bull;&nbsp;&nbsp;Mensaje de la Directora</a></li>
+										<li><a href="LOGROSS">&nbsp;&bull;&nbsp;&nbsp;Nuestros Logros</a></li>
+										<li><a href="ALUMNOS_INTEGRALES">&nbsp;&bull;&nbsp;&nbsp;Alumnos Integrales</a></li>
+										<li><a href="ORGANIGRAMA">&nbsp;&bull;&nbsp;&nbsp;Organigrama</a></li>
+									</ul>
+								</li>
+								<li><a href="#">&nbsp;Programa Educativo</a>
+									<ul>
+										<li><a href="PEIS">&nbsp;&bull;&nbsp;&nbsp;PEI</a></li>
+										<li><a href="NIVELES_EDUCACIONANES">&nbsp;&bull;&nbsp;&nbsp;Niveles de Educacion</a></li>
+										<li><a href="ACTIVIDADES_EXTRAESCOLARES">&nbsp;&bull;&nbsp;&nbsp;Actividades Extraescolares</a></li>
+										<li><a href="REGLAMENTOS">&nbsp;&bull;&nbsp;&nbsp;Reglamentos</a></li>
+									</ul>
+								</li>
+								<li><a href="#">&nbsp;Admiciones</a>
+									<ul>
+										<li><a href="REQUISITOS_INGRESO">&nbsp;&bull;&nbsp;&nbsp;Requisitos de Ingreso</a></li>
+										<li><a href="PREGUNTAS_FRECUENTES">&nbsp;&bull;&nbsp;&nbsp;Preguntas Frecuentes</a></li>
+										<li><a href="CONTACTOS">&nbsp;&bull;&nbsp;&nbsp;Contacto</a></li>
+									</ul>
+								</li>
+								<li><a href="#">&nbsp;Soporte Estudiantil</a>
+									<ul>
+										<li><a href="MATERIALES_APOYOS">&nbsp;&bull;&nbsp;&nbsp;Materiales de Apoyo</a></li>
+										<li><a href="PLAN_LECTOR">&nbsp;&bull;&nbsp;&nbsp;Plan Lector</a></li>
+										<li><a href="UTILES_ESCOLARES">&nbsp;&bull;&nbsp;&nbsp;Lista de Utiles Escolares</a></li>
+										<li><a href="UNIFORME_ESCOLAR">&nbsp;&bull;&nbsp;&nbsp;Uniforme Escolar</a></li>
+										<li><a href="SITIOS_INTERES">&nbsp;&bull;&nbsp;&nbsp;Sitios de interes</a></li>
+									</ul>
+								</li>
+							</ul>
+						</div>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<table id="tablacontenido" ><!--tabla de contenido de la pagina  texto-->
+							<tbody>
+								<tr><!-- icono de la pagina -->
+									<td colspan="7"><a href="index.htm"> <img src="img/logo.jpg"  class="imginicio" alt="error"> </a></td>
+								</tr><!-- logo da vinci -->
+								<!--<tr>
+									<td colspan="7"><a href="index.htm"><img src="img/vineta.jpg" class="imginicio" alt="error"></a></td>
+								</tr>-->
+								<tr>
+									<td id="menulateral"><!-- menu lareral -->
+										<a href="index.htm"><img src="img/comunidad.jpg" class="menulaterallogo"  alt="error"></a>
+										<div class="navlateral">
+											<ul class="navlateral">
+												<li><a href="CALENDARIO">Calendario</a></li>
+												<li><a href="INFORMATIVOS_2018">Informativos</a></li>
+												<li><a href="CENTRO_ALUMNOS">Centro de Alumnos</a></li>
+												<li><a href="CENTRO_PADRES">Centro de Padres</a></li>
+												<li><a href="EX-ALUMNOS">Ex-Alumnos</a></li>
+												<li><a href="CULTURA_MEDIOAMBIENTE">Departamento de Extención Cultura<br>y Medioambiente</a></li>
+												<li><a href="BIENESTAR_CONVIVENCIA">Departamento de Bienestar y Convivencia Escolar</a></li>
+												<li><a href="DEPORTE_RECREACION">Departamento de Deporte, Recreación<br>y Vida Saludable</a></li>
+												<li><a href="LOGROSS">Nuestros Logros</a></li>
+												<li><a href="GALERIA_FOTOS">Galería de Fotos</a></li>
+											</ul>
+										</div>
+										<br>
+										<a href="https://www.sistemadeadmisionescolar.cl/"><img src="img/sae2.jpg" class="imglateral" alt="error"></a>
+										<a href="http://www.demre.cl/"><img src="img/banner_demre.gif" class="imglateral" alt="error"></a>
+										<a href="http://www.icarito.cl"><img src="img/banner_icarito.gif" class="imglateral" alt="error"></a>
+										<a href="http://www.rae.es/"><img src="img/banner_rae.gif" class="imglateral" alt="error"></a>
+										<a href="http://www.educarchile.cl/ech/pro/app/home"><img src="img/banner_educarchile.gif" class="imglateral" alt="error"></a>
+									</td>
---
+++ INFORMACION/piep.php
@@ -0,0 +1,12 @@
+								<tr><!-- pie de pagina -->
+									<td><a href="CONTACTOS" ><img src="img/icono_contacto.jpg" id="imgcontacto" alt="error"></a></td>
+									<td><p id="textofin">Francisco Bilbao 1720 - Pitrufquén - Fono Fax: 45- 39 25 89 - e-mail: <a href="contacto.htm">ldavincischool@gmail.com</a></p></td>
+								</tr>
+							</tbody>
+						</table>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</body>
+</html>
