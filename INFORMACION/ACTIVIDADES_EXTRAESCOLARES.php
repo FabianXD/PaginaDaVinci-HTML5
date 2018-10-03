d1a28454a4aa048748bc8bf853c36265306e42da
FabianXD <fabianma7@gmail.com>
2018-10-03 18:45:03 -0300

    extraescolares

---
+++ INFORMACION/ACTIVIDADES_EXTRAESCOLARES.php
@@ -0,0 +1,251 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Leonardo Da Vinci School - Pitrufquen</title>
+    <meta name="description" content="LEONARDO DA VINCI SCHOOL es un establecimiento educacional mixto, diurno, particular subvencionado, con jornada escolar completa, cuya modalidad de enseñanza es científica - humanista, con énfasis en el idioma Inglés. Imparte educación Pre-básica, Básica y Media, centrada en el respeto a la persona. Está ubicado en la avenida Francisco Bilbao 1720 de la ciudad de Pitrufquén, Novena Región de la Araucanía.">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="manifest" href="site.webmanifest">
+    <link href="../favicon.png" rel="icon" type="image/x-icon" />
+    <link rel="apple-touch-icon" href="../favicon.png">
+    <link rel="stylesheet" href="../css/main.css">
+</head>
+
+<body>
+<?php include('../php/header.php'); ?>
+<td class="tdnoticia">
+    <main>
+        <article id="contenedornoticia">
+            <!-- imagen de noticia -->
+            <div class="imgtitulo">
+                <img src="IMG/icono_titulo.jpg" alt="" srcset="">
+            </div>
+            <!-- titulo de noticia -->
+            <div class="noticia">
+                <p><h3>ACTIVIDADES EXTRAESCOLARES 2017</h3></p>
+            </div>
+            <!-- contenido de la noticia -->
+            <div class="tdnoticia">
+            <table class="tablaremar">
+            <tr>
+                <td COLSPAN="4" class="textalingcenter"><p class="textalingcenter"><b >ACTIVIDADES EXTRAESCOLARES  1° SEM 2017</b></p></td>
+            </tr>
+            <tr>
+                <td>ACTUVUDAD</td>
+                <td>PROFESOR</td>
+                <td>NIVEL</td>
+                <td>DIA Y HORA</td>
+            </tr>
+            <tr>
+                <td>ARTERAPIA: MANDALAS</td>
+                <td>ANDREA GUTIÉRREZ</td>
+                <td>7° BÁSICO a II-MEDIO</td>
+                <td>VIERNES 14:00 – 15:30</td>
+            </tr>
+            <tr>
+                <td>MANUALIDADES CON RECICLAJE</td>
+                <td>MILTON PÉREZ</td>
+                <td>3° a 6° BÁSICO</td>
+                <td>VIERNES 14:00 – 15:30</td>
+            </tr>
+            <tr>
+                <td>TALLER DE CIENCIAS</td>
+                <td>CARLA MARDONES</td>
+                <td>5° a 8° BÁSICO</td>
+                <td>VIERNES 14:00 – 15:30</td>
+            </tr>
+            <tr>
+                <td>BRIGADA ECOLÓGICA</td>
+                <td>ALEJANDRA ROJAS</td>
+                <td>3° a 7° BÁSICO</td>
+                <td>VIERNES 14:00 – 15:30</td>
+            </tr>
+            <tr>
+                <td>PINTURA Y DIBUJO</td>
+                <td>IRIS FUENTES</td>
+                <td>4° BÁSICO a IV-MEDIO</td>
+                <td>VIERNES 14:00 – 15:30</td>
+            </tr>
+            <tr>
+                <td>VIOLÍN</td>
+                <td>CRISTIAN RIQUELME</td>
+                <td>3° BÁSICO a IV-MEDIO</td>
+                <td>VIERNES 14:00 – 15:30 <br> 15:45 - 17:30</td>
+            </tr>
+            <tr>
+                <td>ARTESANÍA EN PLÁSTICO</td>
+                <td>SILVIA VEGA</td>
+                <td>3° a 6° BÁSICO</td>
+                <td>VIERNES 14:00 – 15:30</td>
+            </tr>
+            <tr>
+                <td>DIVERSIÓN ARTÍSTICA</td>
+                <td>CLAUDIA RIQUELME</td>
+                <td>KINDER a 4° BÁSICO</td>
+                <td>VIERNES 14:00 – 15:30</td>
+            </tr>
+            <tr>
+                <td>CREANDO CON MOSTACILLA</td>
+                <td>M TERESA LINEROS</td>
+                <td>2° a 6° BÁSICO</td>
+                <td>VIERNES 14:00 – 15:30</td>
+            </tr>
+            <tr>
+                <td>TELAR</td>
+                <td>PAOLA ARCE</td>
+                <td>3° 6° BÁSICO</td>
+                <td>VIERNES 14:00 – 15:30</td>
+            </tr>
+            <tr>
+                <td>GIMNASIA RÍTMICA (DAMAS)</td>
+                <td>CAROLINA SANZANA</td>
+                <td>K° a 2° BÁSICO SELECCIÓN <br> 3° a 8° BÁSICO</td>
+                <td>MARTES 14:00 - 15:30 <br> MIÉRCOLES 17:15–18:45 <br> VIERNES 14:00 – 15:30</td>
+            </tr>
+            <tr>
+                <td>DANZA (MIXTO)</td>
+                <td>NIBALDO CHÁVEZ</td>
+                <td>6° - 4° MEDIO <br> 6° - 4° MEDIO <br> 6° - 8° BÁSICO</td>
+                <td>LUNES 17:15-18:45 <br> MARTES 17:15-18:45 <br> VIERNES 14:00 – 15:30</td>
+            </tr>
+            <tr>
+                <td>BÁSQUETBOL (MIXTO)</td>
+                <td>FERNANDO ALONSO</td>
+                <td>3° a 6° BÁSICO <br> 7° a IV MEDIO</td>
+                <td>MARTES 17:15 – 18:45 <br> LUNES 17:15 – 18:45</td>
+            </tr>
+            <tr>
+                <td>ZUMBA FITNESS</td>
+                <td>NIBALDO CHÁVEZ</td>
+                <td>1° -8° B</td>
+                <td>MARTES 15:45- 17:15.</td>
+            </tr>
+            <tr>
+                <td>GIMNASIA ARTÍSTICA Y MOTRICIDAD</td>
+                <td>NIBALDO CHÁVEZ</td>
+                <td>KINDER a 2° BÁSICO</td>
+                <td>JUEVES 14:00 – 15:30</td>
+            </tr>
+            <tr>
+                <td>FÚTBOL (VARONES)</td>
+                <td>FERNANDO ALONSO</td>
+                <td>2° a 6° BÁSICO</td>
+                <td>VIERNES 14:00 – 15:30</td>
+            </tr>
+            <tr>
+                <td>FÚTBOL (VARONES)</td>
+                <td>NIBALDO CHÁVEZ</td>
+                <td>7° A 4°M</td>
+                <td>JUEVES 17:15 a 18:45</td>
+            </tr>
+            <tr>
+                <td>KARATE (MIXTO)</td>
+                <td>CLAUDIO MATHEW</td>
+                <td>TODO NIVEL</td>
+                <td>VIERNES 14:00 – 15:30</td>
+            </tr>
+            <tr>
+                <td>VOLEIBOL</td>
+                <td>NIBALDO CHÁVEZ</td>
+                <td>7° -4°M</td>
+                <td>MIÉRCOLES 17:15 – 18:45</td>
+            </tr>
+            <tr>
+                <td>ATLETISMO</td>
+                <td>NIBALDO CHÁVEZ</td>
+                <td>4° a 8° BÁSICO</td>
+                <td>MARTES 14:00 - 15:30</td>
+            </tr>
+            </table>
+            </div>
+            <div>
+            
+						<span class="texto">ARTESAN&Iacute;A EN PL&Aacute;STICO</span>
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/artesania-plastico1.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/artesania-plastico2.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/artesania-plastico3.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/artesania-plastico4.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/artesania-plastico5.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/artesania-plastico6.jpg" class="imgextra">
+						<span class="texto">BRIGADA ECOL&Oacute;GICA</span>
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/brigada-ecologica1.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/brigada-ecologica2.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/brigada-ecologica3.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/brigada-ecologica4.jpg" class="imgextra">
+						<span class="texto">CREANDO CON MOSTACILLA</span>
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/creando-con-mostacilla1.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/creando-con-mostacilla2.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/creando-con-mostacilla3.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/creando-con-mostacilla4.jpg" class="imgextra">
+						<span class="texto">DANZA</span>
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/danza1.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/danza2.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/danza3.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/danza4.jpg" class="imgextra">
+						<span class="texto">DIVERSI&Oacute;N ART&Iacute;STICA</span>
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/diversion-artistica1.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/diversion-artistica2.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/diversion-artistica3.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/diversion-artistica4.jpg" class="imgextra">
+						<span class="texto">FUTBOL</span>
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/futbol1.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/futbol2.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/futbol3.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/futbol4.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/futbol5.jpg" class="imgextra">
+						<span class="texto">GIMNASIA R&Iacute;TMICA</span>
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/gimnasio-ritmica1.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/gimnasio-ritmica2.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/gimnasio-ritmica3.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/gimnasio-ritmica4.jpg" class="imgextra">
+						<span class="texto">KARATE</span>
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/karate1.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/karate2.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/karate3.jpg" class="imgextra">
+						<span class="texto">MANDALAS</span>
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/mandalas1.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/mandalas2.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/mandalas3.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/mandalas4.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/mandalas5.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/mandalas6.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/mandalas7.jpg" class="imgextra">
+						<span class="texto">MANUALIDADES CON RECICLADO</span>
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/manualidades-con-reciclado1.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/manualidades-con-reciclado2.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/manualidades-con-reciclado3.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/manualidades-con-reciclado4.jpg" class="imgextra">
+						<span class="texto">PINTURA Y DIBUJO</span>
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/pinturaydibujo1.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/pinturaydibujo2.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/pinturaydibujo3.jpg" class="imgextra">
+						<span class="texto">TALLER DE CIENCIAS</span>
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/tallerciencias1.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/tallerciencias2.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/tallerciencias3.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/tallerciencias4.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/tallerciencias5.jpg" class="imgextra">
+						<span class="texto">TELAR</span>
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/telar1.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/telar2.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/telar3.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/telar4.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/telar5.jpg" class="imgextra">
+						<span class="texto">VIOL&Iacute;N</span>
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/violin1.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/violin2.jpg" class="imgextra">
+						<img src="IMG/ACTIVIDADES_EXTRAESCOLARES/violin3.jpg" class="imgextra">
+            </div>
+            <div class="tdnoticia">
+                <p class="titulo_noticias3">BENEFICIOS Y BONDADES</p>
+                <p>Una participación efectiva y real beneficia tanto las destrezas de vida como los logros académicos de los alumnos. Dada la diversidad de la oferta, el alumnado debe necesariamente seleccionar y optar por una de ellas - reflejando así una verdadera universidad para niños y adolescentes. Son experiencias socializadoras únicas que contribuyen a la identificación de los alumnos con los valores del colegio. Sus bondades educacionales son múltiples, abarcando áreas tan diversas como notas escolares, tareas, autoestima, alejamiento de las conductas de riesgo e incluso futuras vocaciones profesionales. Sus efectos positivos se prolongan más allá de la vida escolar de los alumnos. Aumentan el nivel de identificación, participación y compromiso con el colegio </p>
+            </div>
+        </article>
+    </main>
+</td>
+</tr>
+<!-- php para pie de pagina -->
+<?php include('../php/pie.php'); ?>
+</body>
+</html>
---
+++ INFORMACION/PREGUNTAS_FRECUENTES.php
@@ -0,0 +1,128 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Leonardo Da Vinci School - Pitrufquen</title>
+    <meta name="description" content="LEONARDO DA VINCI SCHOOL es un establecimiento educacional mixto, diurno, particular subvencionado, con jornada escolar completa, cuya modalidad de enseñanza es científica - humanista, con énfasis en el idioma Inglés. Imparte educación Pre-básica, Básica y Media, centrada en el respeto a la persona. Está ubicado en la avenida Francisco Bilbao 1720 de la ciudad de Pitrufquén, Novena Región de la Araucanía.">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="manifest" href="site.webmanifest">
+    <link href="../favicon.png" rel="icon" type="image/x-icon" />
+    <link rel="apple-touch-icon" href="../favicon.png">
+    <link rel="stylesheet" href="../css/main.css">
+</head>
+
+<body>
+<?php include('../php/header.php'); ?>
+<td class="tdnoticia">
+    <main>
+        <article id="contenedornoticia">
+            <!-- imagen de noticia -->
+            <div class="imgtitulo">
+                <img src="IMG/icono_titulo.jpg" alt="" srcset="">
+            </div>
+            <!-- titulo de noticia -->
+            <div class="noticia">
+                <p><h3>PREGUNTAS FRECUENTES</h3></p>
+            </div>
+            <!-- contenido de la noticia -->
+            <div class="tdnoticia">
+            <div class="nocolor">
+P: Qué debo hacer si mi hijo/a falta al colegio o yo como apoderado a las reuniones y/o citaciones? <br>
+R: Toda inasistencia debe ser justificada personalmente por el apoderado titular o suplente dentro de las 48 horas de la ausencia.
+</div>
+<br>
+<div class="sicolor">
+P:¿Qué debo hacer si mi hijo/a tiene que retirarse antes de la jornada de clases? <br>
+R : Si un alumno debe retirarse durante la jornada de clases, sólo podrá hacerlo si su apoderado titular o suplente viene personalmente a buscarlo firmando el libro correspondiente. <br>
+En caso de que un alumno por problemas de salud inesperado deba retirarse, se le comunicará al apoderado telefónicamente de la situación para que concurra al establecimiento a retirarlo y en caso de alguna dificultad se enviará a su casa con un adulto responsable, previa autorización del apoderado.
+</div>
+<br>
+<div class="nocolor">
+P:¿Qué hacer cuando tengo un problema con mi hijo en el colegio? <br>
+R: Ante cualquier problema que tenga su hijo en el colegio, que requiera ser conversado entre Profesor y Apoderado, se debe seguir el CONDUCTO REGULAR establecido para esto. La línea establecida es primero con el Profesor, luego el Profesor Jefe y finalmente la Dirección del Colegio.
+</div>
+<br>
+<div class="sicolor">
+P: ¿Cómo puedo comunicarme con un profesor por alguna inquietud relacionada con el desempeño académico y/o disciplinario de mi hijo/a? <br>
+R: a)Todos los profesores tienen un horario de atención individual a apoderados, fijado desde el inicio del año escolar e informado. <br>
+b) Los apoderados serán citados por el profesor mediante una comunicación escrita enviada con el alumno, telefónicamente por secretaría o inspectoría o venir libremente en los horarios establecidos. <br>
+c) Los apoderados no deberán venir en horario de clases del profesor ni al inicio ni término de las jornadas. <br>
+d) Las atenciones de apoderados deben ser informadas por escrito a Dirección en formulario existente para ello, informando el motivo de la citación o entrevista y la firma del apoderado.
+</div>
+<br>
+<div class="nocolor">
+P: ¿Qué hacer en caso de que a mi hijo/a le ocurra algún accidente mientras esté en el colegio? <br>
+R: En caso de accidente de un alumno, el colegio procede de la siguiente manera: <br>
+a) se le presta los primeros auxilios. <br>
+b) se da cuenta a Inspectoría o Dirección de la situación. <br>
+c) se hace la declaración de accidente y en forma paralela se le avisa al apoderado de lo acontecido telefónicamente. <br>
+d) Se envía al accidentado al hospital en un taxi con una persona adulta responsable. <br>
+e) el apoderado deberá concurrir al centro asistencial para hacerse cargo de su hijo.<br>
+</div>
+<br>
+<div class="sicolor">
+P: ¿Dónde puede almorzar mi hijo/a en el colegio si vive fuera de la ciudad? <br>
+R: Pese a que el colegio no cuenta con casino ni cocina, existe un espacio físico y microondas para que el alumno caliente su colación traída de la casa si así lo prefiere. Es recomendable usar termos para comida con el fin de optimizar el tiempo.
+</div>
+<br>
+<div class="nocolor">
+P: ¿A quién acudo en caso de que mi hijo/a pierda prendas del uniforme o sus útiles escolares? <br>
+R: Consultar en el colegio si se hubiese quedado en la sala o el patio. El alumno debe hacerse responsable de todas sus pertenencias. Debe tener todo marcado con su nombre y el colegio no se hace responsable de accesorios como teléfonos, pendrives,  computadores personales, etc.
+</div>
+<br>
+<div class="sicolor">
+P: ¿Debe mi hijo/a asistir a clases de extraescolar? <br>
+R : Sí, las clases son obligatorias para todos los alumnos desde 3° básico a 4° año medio, con un porcentaje mínimo de asistencia del 85% para ser promovido de curso.
+</div>
+<br>
+<div class="nocolor">
+P: ¿Qué son las salidas a terreno? <br>
+R: Se consideran salidas a terreno aquellos proyectos interdisciplinarios que incluyen salidas desde una hora de clases hasta tres días, incluyendo Sábado y Domingo. <br>
+Para las salidas a terreno se requiere que: <br>
+a) El profesor responsable de la salida informe a dirección del proyecto a realizar.<br> 
+b) Solicite autorización escrita al apoderado, comunicando con la debida anticipación la salida e informando acerca del lugar a visitar, objetivo, actividad a realizar, hora de inicio y término, puntos de partida y retorno, medio de movilización, adultos responsables. <br>
+c) Entregue en Inspectoría la nómina de alumnos participantes, adjuntando las autorizaciones de los padres. <br>
+d) Registre la salida en el libro de Novedades. <br>
+e) Ningún alumno podrá salir del Colegio sin la autorización escrita del apoderado. <br>
+f) No está permitido en ninguna salida a terreno, cualquiera que esta sea, el consumo de alcohol, ni para estudiantes ni profesor. <br>
+g) El profesor responsable de la salida coordinará el medio de transporte a utilizar. <br>
+h) En caso de que la salida a terreno contemple más de un día, el profesor es el responsable ante la dirección del establecimiento de cumplir con la normativa que exige el Ministerio de Transportes para estos efectos.
+</div>
+<br>
+<div class="sicolor">
+P:¿ Qué cursos están autorizados para usar un polerón especial? ¿Qué condiciones pone el colegio? <br>
+R:a) Sólo los alumnos de cuarto año medio <br>
+b) Deberán ser diseñados por los mismos alumnos en conjunto con su profesor jefe en cuanto a color y leyenda, y la dirección del establecimiento sancionará o aprobará finalmente el diseño elegido.<br> 
+c) El uso del polerón está permitido durante todo el año escolar, siempre que no sea una ocasión formal.
+</div>
+<br>
+<div class="nocolor">
+P: ¿Hasta qué curso los alumnos/as deben usar delantal /cotona obligatoriamente? <br>
+R: El uso del delantal y/o cotona es obligatorio desde Pre kínder hasta Cuarto Año Básico.
+</div>
+<br>
+<div class="sicolor">
+P: ¿El colegio cuenta con movilización propia o convenio con alguna empresa de transportes? <br>
+R: No, el colegio no cuenta con movilización propia para el traslado de los alumnos, ni tiene convenio con ninguna empresa de transporte escolar.
+</div>
+<br>
+<div class="nocolor">
+P:¿Por cuánto tiempo el colegio se hace responsable de los alumnos antes del inicio y después del término de la jornada escolar? <br>
+R: El colegio se hará responsable de los alumnos durante el horario de clases, a excepción de los alumnos de Pre-Kinder y Kínder que será hasta 15 minutos después de la hora de salida de su jornada.
+</div>
+<br>
+<div class="sicolor">
+P: ¿Con qué frecuencia se hacen las reuniones de apoderados? ¿Son obligatorias? <br>
+R: Las reuniones de apoderados se realizan una vez al mes y son calendarizadas por el establecimiento al inicio del año escolar, normalmente la tercera semana del mes. El día y la hora lo convienen entre apoderados y profesor/a. <br>
+Si, son obligatorias. El apoderado debe asistir a ellas al menos en un 75%.
+</div>
+            </div>
+        </article>
+    </main>
+</td>
+</tr>
+<!-- php para pie de pagina -->
+<?php include('../php/pie.php'); ?>
+</body>
+</html>
